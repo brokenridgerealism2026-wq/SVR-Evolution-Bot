5576ca07243ec5167cc8394c85b853000a3b21e0
brokenridgerealism2026-wq <BrokenRidgeRealism2026@gmail.com>
2026-06-24 07:50:15 -0500

    Added command for skin randomizer

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,6 +12,51 @@ const commands = [
         .setName('leaderboard')
         .setDescription('Show the top 10 members with the most accepted evolutionary standing submissions')
         .toJSON(),
+
+new SlashCommandBuilder()
+    .setName('skinrandomizer')
+    .setDescription('Roll hatchling genetics from parent skins and eye colors')
+    .addStringOption(option =>
+        option.setName('mother_dominant')
+            .setDescription('Mother dominant skin - required')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('mother_recessive_1')
+            .setDescription('Mother recessive skin 1 - required')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('father_dominant')
+            .setDescription('Father dominant skin - required')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('father_recessive_1')
+            .setDescription('Father recessive skin 1 - required')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('mother_eyes')
+            .setDescription('Mother eye color - required')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('father_eyes')
+            .setDescription('Father eye color - required')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('mother_recessive_2')
+            .setDescription('Mother recessive skin 2')
+            .setRequired(false))
+    .addStringOption(option =>
+        option.setName('mother_recessive_3')
+            .setDescription('Mother recessive skin 3')
+            .setRequired(false))
+    .addStringOption(option =>
+        option.setName('father_recessive_2')
+            .setDescription('Father recessive skin 2')
+            .setRequired(false))
+    .addStringOption(option =>
+        option.setName('father_recessive_3')
+            .setDescription('Father recessive skin 3')
+            .setRequired(false))
+    .toJSON(),
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
